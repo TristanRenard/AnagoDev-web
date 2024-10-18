5fc3fa43519626b3a00ebb282cf778632009cc45
HugoWeh <hugo.wehrle@supdevinci-edu.fr>
2024-10-18 15:14:03 +0200

    feat(api/categories): Adding category endpoint

---
+++ src/controllers/category.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-nested-ternary */
+import Category from "@/db/models/Category"
+import knexInstance from "@/lib/db"
+
+const categoryController = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" })
+  }
+
+  const { title, description, order, images } = req.body
+
+  if (!title || !description || !order) {
+    return res.status(400).json({
+      message: "Missing required fields",
+      missing: !title ? "title" : !description ? "description" : "order",
+    })
+  }
+
+  try {
+    const response = await Category.query(knexInstance).insert({
+      title,
+      description,
+      images: [],
+      order,
+    })
+
+    return res.status(200).json(response)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ message: "Internal Server Error" })
+  }
+}
+
+export default categoryController
