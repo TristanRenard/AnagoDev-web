9725e114544d1d41a66c4056be17509cedc94f76
Tristan Renard <tristan.tr.renard@gmail.com>
2024-11-09 00:42:01 +0100

    feat(categories): get

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -3,31 +3,37 @@ import Category from "@/db/models/Category"
 import knexInstance from "@/lib/db"
 
 const categoryController = async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method Not Allowed" })
+  if (req.method === "POST") {
+    const { title, description, order, images } = req.body
+
+    if (!title || !description || !order) {
+      return res.status(400).json({
+        message: "Missing required fields",
+        missing: !title ? "title" : !description ? "description" : "order",
+      })
+    }
+
+    try {
+      const response = await Category.query(knexInstance).insert({
+        title,
+        description,
+        images,
+        order,
+      })
+
+      return res.status(200).json(response)
+    } catch (error) {
+      return res.status(500).json({ message: "Internal Server Error" })
+    }
   }
 
-  const { title, description, order, images } = req.body
+  if (req.method === "GET") {
+    const categories = await Category.query(knexInstance).select("*")
 
-  if (!title || !description || !order) {
-    return res.status(400).json({
-      message: "Missing required fields",
-      missing: !title ? "title" : !description ? "description" : "order",
-    })
+    return res.status(200).json(categories)
   }
 
-  try {
-    const response = await Category.query(knexInstance).insert({
-      title,
-      description,
-      images,
-      order,
-    })
-
-    return res.status(200).json(response)
-  } catch (error) {
-    return res.status(500).json({ message: "Internal Server Error" })
-  }
+  return res.status(405).json({ message: "Method Not Allowed" })
 }
 
 export default categoryController
