0da517e5e28fe58883c7e7af1a190ca14edfadd7
TristanRenard <renard.tristan@proton.me>
2025-04-07 16:47:50 +0200

    feat: implement Umami API endpoint and update App component to fetch script dynamically

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,27 +4,49 @@ import { Toaster } from "@/components/ui/toaster"
 import { I18nProvider } from "@/locales"
 import "@/styles/globals.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import axios from "axios"
 import Script from "next/script"
+import { useState } from "react"
 
 const queryClient = new QueryClient()
-const App = ({ Component, pageProps }) => (
-  <>
-    <Script
-      src="http://localhost:30938/script.js"
-      data-website-id="ddb11268-73cc-46eb-901d-06aadcd78d60"
-      strategy="afterInteractive"
-    />
-    <I18nProvider>
-      <QueryClientProvider client={queryClient}>
-        <div className="min-h-screen flex flex-col">
-          <NavBar {...pageProps} />
-          <Component {...pageProps} />
-          <Footer />
-          <Toaster />
-        </div>
-      </QueryClientProvider>
-    </I18nProvider>
-  </>
-)
+const App = ({ Component, pageProps }) => {
+  const [umamiScript, setUmamiScript] = useState(null)
+  const [umamiWebsiteId, setUmamiWebsiteId] = useState(null)
+  axios.get("/api/umami").then((response) => {
+    setUmamiScript(response.data.umamiScript)
+    setUmamiWebsiteId(response.data.umamiWebsiteId)
+  })
+
+  return (
+    <>
+      {umamiScript && umamiWebsiteId && (
+        <Script
+          async
+          defer
+          data-website-id={umamiWebsiteId}
+          src={umamiScript}
+          strategy="afterInteractive"
+          onLoad={() => {
+            console.log("umami loaded")
+          }}
+          onError={(e) => {
+            console.error("Script failed to load", e)
+          }}
+        />
+      )}
+
+      <I18nProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className="min-h-screen flex flex-col">
+            <NavBar {...pageProps} />
+            <Component {...pageProps} />
+            <Footer />
+            <Toaster />
+          </div>
+        </QueryClientProvider>
+      </I18nProvider>
+    </>
+  )
+}
 
 export default App
