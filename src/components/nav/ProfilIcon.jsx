fd5c494a6e5825b68a663cc0dec65d62ec2884a6
Léa Schmitt <146575913+leashmt@users.noreply.github.com>
2025-04-11 13:57:51 +0200

    Update src/components/nav/ProfilIcon.jsx
    Co-authored-by: Tristan Renard <77884392+TristanRenard@users.noreply.github.com>

--- src/components/nav/ProfilIcon.jsx
+++ src/components/nav/ProfilIcon.jsx
@@ -7,6 +7,7 @@ import {
 import { UserRound } from "lucide-react"
 import { useI18n } from "@/locales"
 import Link from "next/link"
+import axios from "axios"
 
 const IconProfil = () => {
   const t = useI18n()
